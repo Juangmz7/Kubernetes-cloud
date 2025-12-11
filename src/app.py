dfad1b0b01167c41f51a45e108d8298ba86dce1e
Juan Gomez <juanjuangmo@gmail.com>
2025-12-11 16:48:53 +0100

    Changed main branch

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # the associated function.
 @app.route('/')
 def hello_world():
-    return 'Hello World, from my Kubernetes project :)'
+    return 'Hello World, from main branch :)'
 
 # main driver function
 if __name__ == '__main__':
