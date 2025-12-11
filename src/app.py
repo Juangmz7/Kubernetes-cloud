9e021884fbace2b592dca3184f8fd1b21de624f4
Juan Gomez <juanjuangmo@gmail.com>
2025-12-11 21:00:45 +0100

    Changed dev

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # the associated function.
 @app.route('/')
 def hello_world():
-    return 'Hello World from production !!!'
+    return 'Hello World from dev !!!'
 
 # main driver function
 if __name__ == '__main__':
