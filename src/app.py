93702675dd44f8c8cc6fbf971c7772393fc2001b
Juan Gomez <juanjuangmo@gmail.com>
2025-12-11 15:28:17 +0100

    Changed code

--- src/app.py
+++ src/app.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 # the associated function.
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return 'Hello World, from my Kubernetes project :)'
 
 # main driver function
 if __name__ == '__main__':
